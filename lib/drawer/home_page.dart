2773d2c1cb287c04994c357c31cd9c150f1c4c22
excelitai-tusher <tusher@excelitai.com>
2022-01-08 16:31:41 +0600

    drawer

---
+++ lib/drawer/home_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[700],
+        title: Center(child: Text("Drawer page")),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 150),
+          child: Column(
+            children: [
+              Center(child: Text("Home Page")),
+            ],
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              MyHeaderDrawer(),
+              MyDrawerList(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Registration page ....
+
+// {
+// "message": "Registration Successfull",
+// "data": {
+// "name": "abirtusher",
+// "email": "user440@gmail.com",
+// "phone": "01758738893",
+// "updated_at": "2022-01-08T04:47:22.000000Z",
+// "created_at": "2022-01-08T04:47:22.000000Z",
+// "id": 23,
+// "profile_photo_url": "https://ui-avatars.com/api/?name=abirtusher&color=7F9CF5&background=EBF4FF"
+// }
+// }
+
+
+// Login page .....
+
+
+// {
+// "message": "Login Successfull",
+// "token": "138|2QAA4jIR8NssEuJZG7ovZ2FY1EnwoSEt5NxuX80P",
+// "data": {
+// "id": 23,
+// "name": "abirtusher",
+// "email": "user440@gmail.com",
+// "provider_id": null,
+// "avatar": null,
+// "phone": "01758738893",
+// "last_seen": null,
+// "email_verified_at": null,
+// "current_team_id": null,
+// "profile_photo_path": null,
+// "created_at": "2022-01-08T04:47:22.000000Z",
+// "updated_at": "2022-01-08T04:47:22.000000Z",
+// "profile_photo_url": "https://ui-avatars.com/api/?name=abirtusher&color=7F9CF5&background=EBF4FF"
+// }
+// }
+
+
+ //user route page.......
+
+// {
+// "id": 23,
+// "name": "abirtusher",
+// "email": "user440@gmail.com",
+// "provider_id": null,
+// "avatar": null,
+// "phone": "01758738893",
+// "last_seen": null,
+// "email_verified_at": null,
+// "current_team_id": null,
+// "profile_photo_path": null,
+// "created_at": "2022-01-08T04:47:22.000000Z",
+// "updated_at": "2022-01-08T04:47:22.000000Z",
+// "profile_photo_url": "https://ui-avatars.com/api/?name=abirtusher&color=7F9CF5&background=EBF4FF"
+// }
--- lib/utility/app_url.dart
+++
@@ -1,7 +0,0 @@
-class AppUrl {
-
-  static const String baseUrl = '';
-
-  static const String Login = baseUrl + '/login';
-  static const String register = baseUrl + '/register';
-}
